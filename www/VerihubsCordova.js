68e57ce05b3a747bbd69886f4fa0b7e702e0d4c0
Andrian <asim.andrian@binus.ac.id>
2020-12-05 17:34:36 +0700

    Initial commit

---
+++ www/VerihubsCordova.js
@@ -0,0 +1,39 @@
+function VerihubsCordova() { }
+
+VerihubsCordova.prototype.initClass = function (
+  successCallback,
+  errorCallback
+) {
+  cordova.exec(successCallback, errorCallback, "VerihubsWrapper", "initClass", null);
+};
+
+VerihubsCordova.prototype.verifyLiveness = function (
+  instructions_count,
+  timeout,
+  successCallback,
+  errorCallback
+) {
+  cordova.exec(
+    successCallback,
+    errorCallback,
+    "VerihubsWrapper",
+    "verifyLiveness",
+    [instructions_count, timeout]
+  );
+};
+
+VerihubsCordova.prototype.getVersion = function (
+  successCallback,
+  errorCallback
+) {
+  cordova.exec(successCallback, errorCallback, "VerihubsWrapper", "getVersion", null);
+};
+
+VerihubsCordova.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.verihubs = new VerihubsCordova();
+  return window.plugins.verihubs;
+};
+cordova.addConstructor(VerihubsCordova.install);
